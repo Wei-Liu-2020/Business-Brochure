27f0c8e0e033f533b36fb78f3efa2585c9cced5d
Wei Liu <liuweileo@gmail.com>
2020-08-06 00:21:21 -0700

    Modified html structure and added JS function for Menu page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-//Show/Hide menu script
+//Show/Hide hamburger menu script
 const body=document.body;
 const btnMenu=document.querySelector('.hamburger');
 
@@ -6,3 +6,46 @@ btnMenu.addEventListener('click',function(e){
    e.preventDefault();
    body.classList.toggle('show');
 });
+
+//Show/Hide menu section script
+document.getElementById("taco-title").addEventListener("click", toggleTacos);
+document.getElementById("starter-title").addEventListener("click", toggleStarters);
+document.getElementById("vegan-title").addEventListener("click", toggleVegans);
+document.getElementById("burrito-title").addEventListener("click", toggleBurritos);
+document.getElementById("drink-title").addEventListener("click", toggleDrinks);
+
+function toggleTacos(){
+   document.getElementById("taco-section").style.display = "block";
+   document.getElementById("starter-section").style.display = "none";
+   document.getElementById("vegan-section").style.display = "none";
+   document.getElementById("burrito-section").style.display = "none";
+   document.getElementById("drink-section").style.display = "none";
+}
+function toggleStarters(){
+   document.getElementById("taco-section").style.display = "none";
+   document.getElementById("starter-section").style.display = "block";
+   document.getElementById("vegan-section").style.display = "none";
+   document.getElementById("burrito-section").style.display = "none";
+   document.getElementById("drink-section").style.display = "none";
+}
+function toggleVegans(){
+   document.getElementById("taco-section").style.display = "none";
+   document.getElementById("starter-section").style.display = "none";
+   document.getElementById("vegan-section").style.display = "block";
+   document.getElementById("burrito-section").style.display = "none";
+   document.getElementById("drink-section").style.display = "none";
+}
+function toggleBurritos(){
+   document.getElementById("taco-section").style.display = "none";
+   document.getElementById("starter-section").style.display = "none";
+   document.getElementById("vegan-section").style.display = "none";
+   document.getElementById("burrito-section").style.display = "block";
+   document.getElementById("drink-section").style.display = "none";
+}
+function toggleDrinks(){
+   document.getElementById("taco-section").style.display = "none";
+   document.getElementById("starter-section").style.display = "none";
+   document.getElementById("vegan-section").style.display = "none";
+   document.getElementById("burrito-section").style.display = "none";
+   document.getElementById("drink-section").style.display = "block";
+}
